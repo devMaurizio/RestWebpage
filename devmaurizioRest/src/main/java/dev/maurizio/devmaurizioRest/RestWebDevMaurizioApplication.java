c4a9f5b02fb75cc9f7f6c1329920e99e4c663313
DevMaurizio <123@123.nl>
2021-12-19 10:50:42 +0100

    setup project Springboot

---
+++ devmaurizioRest/src/main/java/dev/maurizio/devmaurizioRest/RestWebDevMaurizioApplication.java
@@ -0,0 +1,13 @@
+package dev.maurizio.devmaurizioRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebDevMaurizioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebDevMaurizioApplication.class, args);
+	}
+
+}
---
+++ devmaurizioRest/src/test/java/dev/maurizio/devmaurizioRest/RestWebDevMaurizioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maurizio.devmaurizioRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebDevMaurizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
